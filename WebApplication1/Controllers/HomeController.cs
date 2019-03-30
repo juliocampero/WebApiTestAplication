b0b7d9fa83ae1dfa57c3e45c95bbaf34fbfd8c9c
Julio Campero de la Cruz <Julio Campero@jalasoft.local>
2019-03-30 17:31:49 -0400

    Initial COmmit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebApiTestApplication.Models;
+using WebApplication1.Helper;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        StudentAPI _api = new StudentAPI();
+
+        public async Task<IActionResult> Index()
+        {
+            List<Students> student = new List<Students>();
+            HttpClient client = _api.Initial();
+            HttpResponseMessage res = await client.GetAsync("api/Student");
+            if (res.IsSuccessStatusCode)
+            {
+                var result = res.Content.ReadAsStringAsync().Result;
+                student = JsonConvert.DeserializeObject<List<Students>>(result);
+            }
+
+            return View(student);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication1/Helper/StudentAPI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Helper
+{
+    public class StudentAPI
+    {
+        public HttpClient Initial()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:54502");
+            return client;
+        }
+    }
+}
